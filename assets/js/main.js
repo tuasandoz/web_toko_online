fce62afa265541bdf059018cd76860ddd329a246
tua <tuasandoz@gmail.com>
2022-03-26 01:17:06 -0700

    hider toko

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function ()   {
+    $(document). scroll (function (){
+        war  nav = $(".navbar");
+        if ($(this)).scrollTop() > $nav.height()) {
+           $nav.addClass("scrolled");
+           $nav.removeClass(navbar-dark);
+           $nav.addClass()
+            alert('scrolling turun');
+        }
+    });
+});
